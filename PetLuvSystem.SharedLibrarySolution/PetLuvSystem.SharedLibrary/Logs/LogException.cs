177e198f167bf0d7bbf6b71b0953ea17009474b7
NPNKhoa <khoa.nphucnguyen@gmail.com>
2025-01-12 23:14:50 +0700

    except the Logs folder in gitignore file

---
+++ PetLuvSystem.SharedLibrarySolution/PetLuvSystem.SharedLibrary/Logs/LogException.cs
@@ -0,0 +1,22 @@
+﻿using Serilog;
+
+namespace PetLuvSystem.SharedLibrary.Logs
+{
+    public static class LogException
+    {
+        public static void LogExceptions(Exception ex)
+        {
+            Console.WriteLine("Original Exception: ");
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("Exception Message: ");
+            Log.Error(ex.Message);
+        }
+
+        public static void LogInformation(string message)
+        {
+            Console.WriteLine("Info Message: ");
+            Console.WriteLine(message);
+            Log.Information(message);
+        }
+    }
+}
